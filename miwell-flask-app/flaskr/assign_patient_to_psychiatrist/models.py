3ce8cf14a9915374ce356307fd509db0d65b3ef1
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-06 16:00:40 +0100

    Added Psych Patient Relationship table & logic. Still need to write unit tests.

--- miwell-flask-app/flaskr/assign_patient_to_psychiatrist/models.py
+++ miwell-flask-app/flaskr/assign_patient_to_psychiatrist/models.py
@@ -3,7 +3,7 @@
 # Imports --------------------------------------------------------------------------------
 
 from flaskr import db  # import our database instance, stored as 'variable' in our __init__.py file.
-from flaskr.register.models import Patient as PatientTable, Psychiatrist as PsychiatristTable
+from flaskr.register.models import Patient, Psychiatrist
 
 
 # Classes --------------------------------------------------------------------------------
@@ -11,13 +11,13 @@ from flaskr.register.models import Patient as PatientTable, Psychiatrist as Psyc
 class PatientPsychiatristAssign(db.Model):  # Creates the schema for a 'trans-table' within our database.
 
     __tablename__ = 'patient_psychiatrist_assignment_table'
-    patient_username = db.Column(db.String(50), db.ForeignKey(PatientTable.username),primary_key=True),
-    psychiatrist_bacp_number = db.Column(db.String(20), db.ForeignKey(PsychiatristTable.bacp_number))
+    patient_username = db.Column(db.String(50), db.ForeignKey(Patient.username), primary_key=True, unique=True, nullable=False)
+    psychiatrist_bacp_number = db.Column(db.String(20), db.ForeignKey(Psychiatrist.bacp_number), nullable=False)
 
     def __repr__(self):  # Defines the self representation of our data.
         return f'The user {self.patient_username} is assigned to psychiatrist: {self.psychiatrist_bacp_number}.'
 
-    # Flask-Login Method Override ---------------------------------------------------------------
+"""    # Flask-Login Method Override ---------------------------------------------------------------
 
     def get_id(self):  # Overrides the patient's username as our Primary ID with Flask-Login. Probably not necessary.
-        return self.patient_username
+        return self.patient_username"""
