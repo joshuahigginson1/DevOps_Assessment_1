28e1498d3c1e77c07ceeccfaa2b7efce6352f2d8
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-08 23:12:48 +0100

    Just a quick update to the nginx file, where I showcase my custom error handling in Flask.

---
+++ miwell-flask-app/flaskr/dashboard/user_dashboard/read_user_posts.py
@@ -0,0 +1,32 @@
+"""# This is a script dedicated to the read functionality of my mood tracker application.
+
+# Imports
+
+import db
+
+# Functions
+
+
+# A function to query our database, and return the last 14 days of posts.
+
+def return_user_posts(days_to_view):
+
+    for posts in returned_table:
+
+        # First, we need to query our database.
+
+        # Select Date,
+
+
+    def has_psychiatrist_made_comment():
+
+        # Query our database, or currently existing fields. Either one.
+
+        if:
+            pass
+
+        else:
+            # Display the psychatrist comment.
+
+
+"""
