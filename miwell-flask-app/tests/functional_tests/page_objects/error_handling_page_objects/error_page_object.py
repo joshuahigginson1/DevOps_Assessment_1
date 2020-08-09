20923192ea83ed6532a8d0411883df26209ba40a
Josh Higginson <joshuahigginson1@gmail.com>
2020-08-10 00:07:25 +0100

    Page Objects for nav bar done.

--- miwell-flask-app/tests/functional_tests/page_objects/error_handling_page_objects/error_page_object.py
+++ miwell-flask-app/tests/functional_tests/page_objects/error_handling_page_objects/error_page_object.py
@@ -9,14 +9,49 @@ from tests.functional_tests.page_objects.common_page_objects import CommonPageOb
 
 class HomePageObject(CommonPageObject):
 
-    def random_error_remark(self):
-        return self.test.client.find_element_by_xpath('/html/body/h1')
+    def get_random_error_remark(self):
+
+        get_page_element = self.test.client.find_element_by_xpath('/html/body/h1')
+
+        attributes = {
+            'page label': get_page_element.get_attribute('innerHTML'),
+            'page element': get_page_element
+        }
+
+        return attributes
 
     def error_code_response(self):
-        return self.test.client.find_element_by_xpath('/html/body/p')
+        get_page_element = self.test.client.find_element_by_xpath('/html/body/p')
+
+        attributes = {
+            'page label': get_page_element.get_attribute('innerHTML'),
+            'page element': get_page_element
+        }
+
+        return attributes
 
     def random_error_photo(self):
-        return self.test.client.find_element_by_xpath('/html/body/img')
+        get_page_element = self.test.client.find_element_by_xpath('/html/body/img')
+
+        attributes = {
+            'page label': get_page_element.get_attribute('innerHTML'),
+            'page element': get_page_element
+        }
+
+        return attributes
 
     def get_back_button(self):
-        return self.test.client.find_element_by_xpath('/html/body/a')
+        get_page_element = self.test.client.find_element_by_xpath('/html/body/a')
+
+        attributes = {
+            'button label': get_page_element.get_attribute('innerHTML'),
+            'button element': get_page_element
+        }
+
+        return attributes
+
+    def click_back_button(self):
+        get_back_button = self.get_back_button()['button element']
+        get_back_button.click()  # Simulates a button click.
+
+
--- miwell-flask-app/tests/functional_tests/page_objects/user_greeting_page_object.py
+++ miwell-flask-app/tests/functional_tests/page_objects/user_greeting_page_object.py
@@ -8,4 +8,21 @@ from tests.functional_tests.page_objects.common_page_objects import CommonPageOb
 
 
 class UserGreetingPageObject(CommonPageObject):
-    pass
+
+    def greeting_header(self):
+        return self.test.client.find_element_by_xpath('/html/body/h1')
+
+    def email_label(self):
+        return self.test.client.find_element_by_xpath('//*[@id="email"]')
+
+    def password_textbox(self):
+        return self.test.client.find_element_by_xpath('//*[@id="password"]')
+
+    def remember_me_checkbox(self):
+        return self.test.client.find_element_by_xpath('//*[@id="remember"]')
+
+    def login_button(self):
+        return self.test.client.find_element_by_xpath('//*[@id="submit"]')
+
+    def register_link(self):
+        return self.test.client.find_element_by_xpath('/html/body/a')
